fb06109c2cd8dc719b2adbee2db66895ccef8d0e
Ethan Cheatham <ethanwcheatham@gmail.com>
2020-02-06 18:31:29 -0800

    maps rendering

--- App.tsx
+++ App.tsx
@@ -1,43 +1,74 @@
-import Launch from './src/components/Launch/Launch';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import Items from './src/containers/Item/Items';
-import CreateItem from './src/components/CreateItem';
-import {Text, Alert, Button} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 
-const AppNavigator = createStackNavigator({
-  LaunchScreen: {
-    screen: Launch,
-    navigationOptions: {
-      gesturesEnabled: false,
-      header: null,
-      title: 'Test title',
-    },
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 600,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
-  Items: {
-    screen: Items,
-    navigationOptions: {
-      title: 'Grocery List',
-      headerRight: () => (
-        <Button
-          onPress={() => Alert.alert('This is a button!')}
-          title="Info"
-          color="red"
-        />
-      ),
-      headerStyle: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-      },
-    },
-  },
-  CreateItem: {
-    screen: CreateItem,
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
-const App = createAppContainer(AppNavigator);
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      region={{
+        latitude: 39.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      }}></MapView>
+  </View>
+);
 
-export default App;
+// import Launch from './src/components/Launch/Launch';
+// import {createAppContainer} from 'react-navigation';
+// import {createStackNavigator} from 'react-navigation-stack';
+// import Items from './src/containers/Item/Items';
+// import CreateItem from './src/components/CreateItem';
+// import {Text, Alert, Button} from 'react-native';
+// import React from 'react';
+
+// const AppNavigator = createStackNavigator({
+//   LaunchScreen: {
+//     screen: Launch,
+//     navigationOptions: {
+//       gesturesEnabled: false,
+//       header: null,
+//       title: 'Test title',
+//     },
+//   },
+//   Items: {
+//     screen: Items,
+//     navigationOptions: {
+//       title: 'Grocery List',
+//       headerRight: () => (
+//         <Button
+//           onPress={() => Alert.alert('This is a button!')}
+//           title="Info"
+//           color="red"
+//         />
+//       ),
+//       headerStyle: {
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         alignContent: 'center',
+//       },
+//     },
+//   },
+//   CreateItem: {
+//     screen: CreateItem,
+//   },
+// });
+
+// const App = createAppContainer(AppNavigator);
+
+// export default App;
