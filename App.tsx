e5fe79d728a87ea451803d829932e69a745faf67
Ethan Cheatham <ethanwcheatham@gmail.com>
2020-02-06 19:04:20 -0800

    map and loc

--- App.tsx
+++ App.tsx
@@ -1,74 +1,43 @@
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-import {StyleSheet, View} from 'react-native';
+import Launch from './src/components/Launch/Launch';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Items from './src/containers/Item/Items';
+import CreateItem from './src/components/CreateItem';
+import {Text, Alert, Button} from 'react-native';
 import React from 'react';
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 600,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+const AppNavigator = createStackNavigator({
+  LaunchScreen: {
+    screen: Launch,
+    navigationOptions: {
+      gesturesEnabled: false,
+      header: null,
+      title: 'Test title',
+    },
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
+  Items: {
+    screen: Items,
+    navigationOptions: {
+      title: 'Grocery List',
+      headerRight: () => (
+        <Button
+          onPress={() => Alert.alert('This is a button!')}
+          title="Info"
+          color="red"
+        />
+      ),
+      headerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+      },
+    },
+  },
+  CreateItem: {
+    screen: CreateItem,
   },
 });
 
-export default () => (
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      region={{
-        latitude: 39.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }}></MapView>
-  </View>
-);
+const App = createAppContainer(AppNavigator);
 
-// import Launch from './src/components/Launch/Launch';
-// import {createAppContainer} from 'react-navigation';
-// import {createStackNavigator} from 'react-navigation-stack';
-// import Items from './src/containers/Item/Items';
-// import CreateItem from './src/components/CreateItem';
-// import {Text, Alert, Button} from 'react-native';
-// import React from 'react';
-
-// const AppNavigator = createStackNavigator({
-//   LaunchScreen: {
-//     screen: Launch,
-//     navigationOptions: {
-//       gesturesEnabled: false,
-//       header: null,
-//       title: 'Test title',
-//     },
-//   },
-//   Items: {
-//     screen: Items,
-//     navigationOptions: {
-//       title: 'Grocery List',
-//       headerRight: () => (
-//         <Button
-//           onPress={() => Alert.alert('This is a button!')}
-//           title="Info"
-//           color="red"
-//         />
-//       ),
-//       headerStyle: {
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         alignContent: 'center',
-//       },
-//     },
-//   },
-//   CreateItem: {
-//     screen: CreateItem,
-//   },
-// });
-
-// const App = createAppContainer(AppNavigator);
-
-// export default App;
+export default App;
--- src/components/CreateItem.tsx
+++ src/components/CreateItem.tsx
@@ -1,9 +1,20 @@
 import React, {useState} from 'react';
-import {View, Picker, FlatList, TextInput, Button, Text} from 'react-native';
+import {
+  View,
+  Picker,
+  FlatList,
+  TextInput,
+  Button,
+  Text,
+  Alert,
+  StyleSheet,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {Colors} from '../styles';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import ImagePicker from 'react-native-image-picker';
+import Geolocation from '@react-native-community/geolocation';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
 const CreateItem = (props: any) => {
   const [language, setLaunguage] = useState('');
@@ -45,6 +56,10 @@ const CreateItem = (props: any) => {
 
   const mydate = new Date('2020-06-12T14:42:42');
   const mode = 'date';
+
+  const getLoc = () => {
+    Geolocation.getCurrentPosition(info => Alert.alert(JSON.stringify(info)));
+  };
 
   const options = {
     title: 'Select Avatar',
@@ -90,88 +105,113 @@ const CreateItem = (props: any) => {
   const pickerItems = DATA.map((i: any) => (
     <Picker.Item label={i.id} value={i.id} />
   ));
+
+  const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+      height: 400,
+      width: 400,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+  });
+
   return (
-    <View style={{flex: 1, paddingLeft: 10, paddingRight: 10}}>
-      <Picker
-        selectedValue={language}
-        onValueChange={(itemValue, itemIndex) => setLaunguage(itemValue)}>
-        {pickerItems}
-      </Picker>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}>
-        <TextInput value={''} placeholder={'Task Name'} style={{flex: 9}} />
-        <Icon
-          name="close"
-          size={25}
-          color={Colors.Colors.three}
-          style={{backgroundColor: 'red', alignItems: 'center'}}
-        />
-      </View>
-      <TextInput value={''} placeholder={'Date & Time'} />
-      {/* <DateTimePicker
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+      <Button title="loc" onPress={() => getLoc()} />
+      <View style={{flex: 1, paddingLeft: 10, paddingRight: 10}}>
+        <Picker
+          selectedValue={language}
+          onValueChange={(itemValue, itemIndex) => setLaunguage(itemValue)}>
+          {pickerItems}
+        </Picker>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}>
+          <TextInput value={''} placeholder={'Task Name'} style={{flex: 9}} />
+          <Icon
+            name="close"
+            size={25}
+            color={Colors.Colors.three}
+            style={{backgroundColor: 'red', alignItems: 'center'}}
+          />
+        </View>
+        <TextInput value={''} placeholder={'Date & Time'} />
+        {/* <DateTimePicker
         value={mydate}
         mode={mode}
         is24Hour={true}
         display="default"
         onChange={() => console.log()}
       /> */}
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          paddingHorizontal: 5,
-        }}>
-        <Icon
-          name="camera"
-          size={25}
-          color={Colors.Colors.three}
-          style={{alignItems: 'center'}}
-          onPress={() => lll()}
-        />
-        <Icon
-          name="photo"
-          size={25}
-          color={Colors.Colors.three}
-          style={{alignItems: 'center'}}
-          onPress={() => ll()}
-        />
-        <Icon
-          name="map"
-          size={25}
-          color={Colors.Colors.three}
-          style={{alignItems: 'center'}}
-        />
-        <Icon
-          name="star"
-          size={25}
-          color={Colors.Colors.three}
-          style={{alignItems: 'center'}}
-        />
-        <Icon
-          name="user"
-          size={25}
-          color={Colors.Colors.three}
-          style={{alignItems: 'center'}}
-        />
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: 5,
+          }}>
+          <Icon
+            name="camera"
+            size={25}
+            color={Colors.Colors.three}
+            style={{alignItems: 'center'}}
+            onPress={() => lll()}
+          />
+          <Icon
+            name="photo"
+            size={25}
+            color={Colors.Colors.three}
+            style={{alignItems: 'center'}}
+            onPress={() => ll()}
+          />
+          <Icon
+            name="map"
+            size={25}
+            color={Colors.Colors.three}
+            style={{alignItems: 'center'}}
+          />
+          <Icon
+            name="star"
+            size={25}
+            color={Colors.Colors.three}
+            style={{alignItems: 'center'}}
+          />
+          <Icon
+            name="user"
+            size={25}
+            color={Colors.Colors.three}
+            style={{alignItems: 'center'}}
+          />
+        </View>
+        <View
+          style={{
+            backgroundColor: 'red',
+            width: 300,
+            height: 75,
+            alignSelf: 'center',
+            marginTop: 50,
+            borderRadius: 50,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: 'white'}}>ADD TASK</Text>
+        </View>
       </View>
-      <View
-        style={{
-          backgroundColor: 'red',
-          width: 300,
-          height: 75,
-          alignSelf: 'center',
-          marginTop: 50,
-          borderRadius: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <Text style={{color: 'white'}}>ADD TASK</Text>
-      </View>
-
     </View>
   );
 };
